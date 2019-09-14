b155067bc9b77d3e1d762c536884f8fcb98220e8
Keith Isham <kisham8899@gmail.com>
2019-09-14 15:56:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,9 +29,11 @@ end
 		erb :login
 	end
 
-	post "/login" do
+post "/login" do
   user = User.find_by(:username => params[:username])
-  if user
+ 
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
     redirect "/success"
   else
     redirect "/failure"
